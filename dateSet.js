59b56c1faf652495893effd728ecee7fe55dcae5
amiminmin <37482441+amiminmin@users.noreply.github.com>
2019-06-05 18:29:40 +0900

    Add files via upload

--- dateSet.js
+++ dateSet.js
@@ -1,4 +1,15 @@
 $(function () {
     //デートピッカー用スクリプト
-    $("#datepicker").pickadate();
+    $("#datepicker").pickadate({
+        //ボタン類
+        today: '本日',
+        clear: '',
+        close: '閉じる',
+        //フォーマット
+        format: 'm月d日(ddd)',
+        formatSubmit: 'yyyymm-dd',
+        hiddenName: true,
+        //自動で閉じない
+        closeOnSelect: false,
+    });
 });
