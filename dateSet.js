4dd7508e0397e6e02aea2998cb01698455fa29fc
amiminmin <37482441+amiminmin@users.noreply.github.com>
2019-06-06 11:52:44 +0900

    結構修正した
    ・input type="date"系のフォームをなくした
    ・pickadate.js-3.6.2以下の整理、ja_JP.jsの階層を上げた
    ・日付valueの裏処理を実装
    ・sp版、css準備

--- dateSet.js
+++ dateSet.js
@@ -1,5 +1,5 @@
 $(function () {
-    //デートピッカー用スクリプト
+    //日付処理
     $("#datepicker").pickadate({
         //ボタン類
         today: '本日',
@@ -8,8 +8,23 @@ $(function () {
         //フォーマット
         format: 'm月d日(ddd)',
         formatSubmit: 'yyyymm-dd',
-        hiddenName: true,
+        hiddenSuffix: 'js_gethiduke',
         //自動で閉じない
         closeOnSelect: false,
     });
+    var thisName = 'input[name=js_gethiduke]';
+    //id属性を追加しname属性を削除
+    $(thisName).attr('id', 'js_gethiduke');
+    $(thisName).removeAttr('name');
+    //
 });
+
+function sethiduke() {　//カレンダーをタップor検索ボタンタップで発火
+    //idからvalueを取得し、分割してinput:hiddenに渡す
+    var date = $('#js_gethiduke').val().split('-');
+    var Dym = date[0];
+    var Ddd = date[1];
+    $('input[name=Dym]').val(Dym);
+    $('input[name=Ddd]').val(Ddd);
+    return;
+};
